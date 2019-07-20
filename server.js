1802773948079516401253fbf3ae18aaa3414b12
Yogasmara <septianayoga30@gmail.com>
2019-07-20 15:02:05 +0700

    finishing register route

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 const PORT = 3000;
 
+//Saving data as array
+const users = [];
+
 app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 //Make a route for home
 app.get('/', (req, res) => {
@@ -14,9 +19,31 @@ app.get('/login', (req, res) => {
   res.render('login.ejs');
 });
 
+//receiving data from login
+app.post('/login', (req, res) => {
+
+});
+
 //make a route for register
 app.get('/register', (req, res) => {
   res.render('register.ejs');
+});
+
+//receiving data from register
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10); //make password hashed 10 times
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    });
+    res.redirect('/login');
+  } catch {
+    //if the register fail, redirect back to register page
+    res.redirect('/register');
+  }
 });
 
 app.listen(PORT);
