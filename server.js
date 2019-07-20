852d782ca188563887d18dce32c9bc460cac9854
Yogasmara <septianayoga30@gmail.com>
2019-07-20 14:30:43 +0700

    fixing route

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const PORT = 3000;
 
 app.set('view-engine', 'ejs');
 
+//Make a route for home
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
+//make a route for login
 app.get('/login', (req, res) => {
   res.render('login.ejs');
 });
 
+//make a route for register
 app.get('/register', (req, res) => {
-  res.render('register.ejs')
+  res.render('register.ejs');
 });
 
 app.listen(PORT);
