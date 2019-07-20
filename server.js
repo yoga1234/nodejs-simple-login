0433735c9742a4e524d7da55b9068fa26c7b33b7
Yogasmara <septianayoga30@gmail.com>
2019-07-20 13:21:40 +0700

    Adding a route

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.set('view-engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index.ejs');
 });
 
-app.listen(3000);
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+});
+
+app.listen(PORT);
